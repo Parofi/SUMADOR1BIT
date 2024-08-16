e66775c7aa75f40d1ddb919425e6da236990960e
Parofi <paulinorobledo@gmail.com>
2024-08-15 19:34:48 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_SUMADOR1BIT (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,17 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+wire s1,cl,s2;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    C_XOR U1 (.OUT(s1), .A(ui_in[0]), .B(ui_in[1]));
+    C_AND U2 (.OUT(cl), .A(ui_in[0]), .B(ui_in[1]));
+    C_XOR U3 (.OUT(uo_out[0]), .A(sl), .B(ui_in[2]));
+    C_AND U4 (.OUT(s2), .A(sl), .B(ui_in[2]));
+    C_XOR U5 (.OUT(uo_out[1]), .A(s2), .B(cl));
 
+    assign uo_out [7:2] = 6'b0;
+    assign uio_out [7:0] = 8'b0;
+    assign uio_oe [7:0] = 8'b0;
+
+    wire _unused = &{ena, clk, rst_n, ui_in [7:3], uio_in [7:0], 1'b0};
 endmodule
